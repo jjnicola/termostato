aa26fbdb15c5806c47509ed5daaa50c2929cc270
Juan Jose Nicola <jjnicola@gmail.com>
2020-10-18 13:04:25 +0200

    When the fridge starts or stops, the motor consumes some power, and a lack on the circuit design, or even some induction produces the selected state to increase in one or 2 posistion (like somebody pressing the style selector button). To avoid this, the rb0 (style selector button) is disable.

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * File:   main.c
  * Author: Juan Jose Nicola
  *
@@ -28,6 +28,11 @@
 
 #define ON         1
 #define OFF        0
+
+/** Being conservative, in case of failure the best cases is to low down
+the temperature. Better cold, green apple, stop fermentation which can be restarted later
+than fusels which is not possible to eliminate. Therefore the fridge is "normal ON" when there is
+no signal from the controler, and must be explicitely OFF with a signal to leave the temp to reise up.*/
 #define FRIDGE_OFF 1
 #define FRIDGE_ON  0
 
@@ -88,24 +93,25 @@ int main (int argc, char** argv)
   int counterloop = 0;
   unsigned char a;
   int fridge_state = 0;
+  int fridge_change_state_wait_count = 0;
 
   lcd_init ();
   lcd_show_lines ("  Ammann", "Cerveceria");
   delay_ms (3000);
   style_set = ENGLISH;
   custom_temp = temp[style_set];
-  
+
   /* Init serial port. */
   init_comms ();
   delay_ms (100);
   TRISCbits.TRISC5 = 0;  //Set port C as output port.
-  
+
   /* Set interruptions */
   PEIE = ON;     // Enabel perisferic interruption.
-  INTF = OFF;    //Clean interruption flag for RB0.
+  INTF = OFF;    // Clean interruption flag for RB0.
   INTE = ON;     // Enable interruption for RB0.
-  INTEDG = ON;  //Set ascendent edge interruption on rb0.
-  GIE = ON;      //Enable global interruption.
+  INTEDG = ON;   // Set ascendent edge interruption on rb0.
+  GIE = ON;      // Enable global interruption.
 
   /* Begin infinite loop. */
   while(1)
@@ -131,7 +137,7 @@ int main (int argc, char** argv)
 
       val2temp ((int)temp_read, buffer_temp);
       lcd_show_lines ("Temp. Actual", buffer_temp);
-      
+
       /* Set the new style or custom temperature. */
       if (new_cmd == 1)
         {
@@ -140,56 +146,83 @@ int main (int argc, char** argv)
           cmdcount = 0;
           lcd_show_lines ("New setup", buffercmd);
           selcmd = cmdchk (buffercmd);
-          if (selcmd == TMP )
-            {          
-              custom_temp = cmd2int (buffercmd);
-              style_set = CUSTOM;
-              lcd_show_lines ("Configure\0", buffercmd);
-            }
-          if (selcmd == STY)
-            {
-              style_set = cmd2int (buffercmd);
-              lcd_show_lines ("New Style\0", style[style_set]);
-            }
-          if (selcmd == RST)
-            {
-              MAX = 0;
-              MIN = 0;
-              style_set = ENGLISH;
-              lcd_show_lines ("Reset\0", "...\0");
-            }
           if (selcmd == GTL)
             {
               val2temp ((int)temp_read, log_data);
               log_data[5] = 48 + RC5;
               comm_send_log (log_data);
             }
-          if (selcmd == GTS)
+          else if (selcmd == STY)
+            {
+              style_set = cmd2int (buffercmd);
+              lcd_show_lines ("New Style\0", style[style_set]);
+            }
+          else if (selcmd == RST)
+            {
+              MAX = 0;
+              MIN = 0;
+              style_set = ENGLISH;
+              lcd_show_lines ("Reset\0", "...\0");
+            }
+          else if (selcmd == TMP )
+            {
+              custom_temp = cmd2int (buffercmd);
+              style_set = CUSTOM;
+              lcd_show_lines ("Configure\0", buffercmd);
+            }
+          else if (selcmd == GTS)
             {
               val2temp (custom_temp, log_data);
               log_data[5] = 48 + style_set;
               comm_send_log (log_data);
             }
         }
+
+      /* Check if the rb0 interruption is disables and if it must be enabled again.*/
+      if (INTE == OFF)
+        {
+          fridge_change_state_wait_count--;
+
+          /* After five time, the rb0 interruption is enabled again.*/
+          if (fridge_change_state_wait_count == 0)
+            INTE = ON;
+        }
+
       /* Compare temperatures and set the fridge on or off.
        * It takes in count the hysteresis to avoid start and stop
        * the fridge in short time periods.
+       * When the fridge starts or stops, the motor consumes some power,
+       * and a lack on the circuit design, or even some induction produces
+       * the selected state to increase in one or 2 posistion (like somebody pressing
+       * the style selector button).
+       * To avoid this, the rb0 (style selector button) is disable.
        */
       if (fridge_state == FRIDGE_OFF)
         {
           if (temp_read > (custom_temp + hyst))
-            RC5 = FRIDGE_ON; 
-          fridge_state = FRIDGE_ON;
+            {
+              // Disable rb0 interruption
+              INTE = OFF;
+
+              RC5 = FRIDGE_ON;
+              fridge_state = FRIDGE_ON;
+              fridge_change_state_wait_count = 5;
+
+            }
         }
       else
-        { 
+        {
           if (temp_read < (custom_temp - hyst))
-            RC5 = FRIDGE_OFF;
-            fridge_state = FRIDGE_OFF;
-        }
+            {
+              // Disable rb0 interruption
+              INTE = OFF;
 
-      
-    } /* End loop infinito */
+              RC5 = FRIDGE_OFF;
+              fridge_state = FRIDGE_OFF;
+              fridge_change_state_wait_count = 5;
+            }
+        }
+    } /* End infinit loop */
 
   return (EXIT_SUCCESS);
 }
